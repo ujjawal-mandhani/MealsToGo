adebb08f839c43e2e631bfdd0fb12626c84a6336
ujjawal-mandhani <9918103237@mail.jiit.ac.in>
2021-05-27 01:47:29 +0530

    day1

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import React from 'react';
+import {StatusBar, StyleSheet, SafeAreaView, Text, View, Platform } from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
+// console.log(StatusBar.currentHeight);
+export default function App() {
+  return (
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight}}>
+        <View style={{ padding: 16, backgroundColor: "green"}}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "blue"}}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
