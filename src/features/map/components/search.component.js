bd0e221fb128cd2ae33df548003f787bbf842f07
ujjawal-mandhani <9918103237@mail.jiit.ac.in>
2021-06-14 02:12:47 +0530

    updating maps

--- src/features/map/components/search.component.js
+++ src/features/map/components/search.component.js
@@ -5,8 +5,11 @@ import { LocationContext } from "../../../services/location/location.context";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
+  position: absolute;
+  z-index: 999;
+  top: 40px;
+  width: 100%;
 `;
-
 export const Search = () => {
   const { keyword, search } = useContext(LocationContext);
   const [searchKeyword, setSearchKeyword] = useState(keyword);
@@ -17,6 +20,7 @@ export const Search = () => {
     <SearchContainer>
       <Searchbar
         placeholder="Search for a location"
+        icon="map"
         value={searchKeyword}
         onSubmitEditing={() => {
           search(searchKeyword);
