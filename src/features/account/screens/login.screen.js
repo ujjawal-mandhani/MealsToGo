671f2dda11ffb4bd1a40aa518af245bc5d6c7d89
ujjawal-mandhani <9918103237@mail.jiit.ac.in>
2021-06-18 02:11:35 +0530

    successful working login

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -1,6 +1,55 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Text } from "../../../components/typography/text.component";
 import { AccountBackground } from "../components/account.styles";
+import { AccountCover } from "../components/account.styles";
+import { AccountContainer } from "../components/account.styles";
+import { AuthButton } from "../components/account.styles";
+import { AuthInput } from "../components/account.styles";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 export const LoginScreen = () => {
-  return <AccountBackground />;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { onLogin, error } = useContext(AuthenticationContext);
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <AccountContainer>
+        <AuthInput
+          label="E-mail"
+          value={email}
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={(u) => setEmail(u)}
+        />
+        <Spacer size="large">
+          <AuthInput
+            label="Password"
+            value={password}
+            textContentType="password"
+            secureTextEntry
+            autoCapitalize="none"
+            secure
+            onChangeText={(p) => setPassword(p)}
+          />
+        </Spacer>
+        {error && (
+          <Spacer size="large">
+            <Text variant="error">{error}</Text>
+          </Spacer>
+        )}
+        <Spacer size="large">
+          <AuthButton
+            icon="lock-open-outline"
+            mode="contained"
+            onPress={() => onLogin(email, password)}
+          >
+            Login
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
+    </AccountBackground>
+  );
 };
